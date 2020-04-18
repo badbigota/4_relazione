970d431fe7714b6f8fca0f4b7af193a8f839f4b5
Tommaso Bertola <bertola.tommaso@hotmail.it>
2020-04-18 15:17:24 +0200

    rename comp

--- STATISTICA.h
+++ STATISTICA.h
@@ -109,7 +109,7 @@ double dstd_media(vector<double> dati, int inizio = 0, int fine = 0)
 }
 
 //Compatibilità avendo due valori medi e i sigma relativi
-double compatibility(double a, double b, double sigma_a, double sigma_b)
+double comp(double a, double b, double sigma_a, double sigma_b)
 {
     return abs(a - b) / sqrt(pow(sigma_a, 2) + pow(sigma_b, 2));
 }
